6d2383f16f2c22600ebbf2b524abfc0837e4a708
Michael Gundlach <gundlach@gmail.com>
2017-06-17 01:29:49 +0000

    CRUD working, could use cleaner API code if I cared

--- server/__init__.py
+++ server/__init__.py
@@ -10,19 +10,17 @@ sqlite_file = os.path.join(BASE_DIR, "cards.sqlite")
 
 def db():
     conn = sqlite3.connect(sqlite_file)
+    conn.row_factory = sqlite3.Row
     return (conn, conn.cursor())
-
-# Project against SQL injection: only alphanum chars allowed in
-# sanitized user inputted column names.
-def clean_colname(colname):
-    return ''.join(char for char in colname if char.isalnum())
 
 def update_fields_sql(card):
     """Return 'a=b, c=d, e=f' for the keys and values in the given |card|'s
     data dict.  Clean column names to protect against sqlinjection."""
-    # TODO hard coded to han|pinyin|english|errors, maybe fine
-    # if we ever expand this, use clean_colname on the colnames.
-    return ("han=?,pinyin=?,english=?,errors=?,id=?", (card['data']['han'], card['data']['pinyin'], card['data']['english'], repr(card['errors']), card['id']))
+    # TODO hard coded to han|pinyin|english|errors, maybe fine.
+    # If we ever read column names from the card (aka unsafe user input),
+    # be sure to convert colnames to ''.join(c for c in colname if c.isalnum())
+    return ("han=?,pinyin=?,english=?,pack_name=?,errors=?", (card['han'], card['pinyin'], card['english'], card['pack_name'], repr(card['errors'])))
+
 
 @app.route("/cards/", methods=["GET"])
 def cards():
@@ -30,16 +28,15 @@ def cards():
     c.execute('SELECT * from cards')
     all_rows = c.fetchall()
     conn.close()
-    return repr(all_rows)
+    return jsonify([dict(zip(row.keys(), row)) for row in all_rows])
 
 @app.route("/cards/", methods=["POST"])
 def insert_card():
     card = request.get_json(force=True)
     conn, c = db()
     now = unicode(datetime.datetime.utcnow())
-    sql = "INSERT INTO cards (han,pinyin,english,errors,created_date) VALUES (?,?,?,?,?)"
-    data = card['data']
-    vals = [card['data'][k] for k in ['han', 'pinyin', 'english']]
+    sql = "INSERT INTO cards (han,pinyin,english,pack_name,errors,created_date) VALUES (?,?,?,?,?,?)"
+    vals = [card[k] for k in ['han', 'pinyin', 'english', 'pack_name']]
     vals.append(unicode(repr(card['errors'])))
     vals.append(now)
     c.execute(sql, vals)
