d0d42a113876b115d083c12073b90f2725b5393b
Michael Gundlach <gundlach@gmail.com>
2017-06-21 14:23:59 +0000

    PacksViewer namespace

--- reviewer/script.js
+++ reviewer/script.js
@@ -1,59 +1,63 @@
 // List of packs (which are lists of cards), loaded from the backend
 PACKS = [];
 
-const virtualPacks = [
-  {title: "All Cards", name: "__all"},
-  {title: "Multiple Packs (TODO)", name: "__multiple"},
-  {title: "Cards With Errors", name: "__wrongs"}
-];
+PacksViewer = function() {}
+PacksViewer.__proto__ = {
 
-// Return an <input> you can click to review a pack.
-function buttonForPack(title, packName) {
-  return $("<input>", {value: title, type: "button"}).
-    addClass("pack").
-    click(() => showPack(packName));
-}
+  virtualPacks: [
+    {title: "All Cards", name: "__all"},
+    {title: "Multiple Packs (TODO)", name: "__multiple"},
+    {title: "Cards With Errors", name: "__wrongs"}
+  ],
 
-function showPack(packName) {
-  if (packName.indexOf("__") === 0)
-    showVirtualPack(packName);
-  else
-    showActualPack(packName);
-}
+  // Return an <input> you can click to review a pack.
+  buttonForPack: function(title, packName) {
+    return $("<input>", {value: title, type: "button"}).
+      addClass("pack").
+      click(() => this.showPack(packName));
+  },
 
-function showVirtualPack(packName) {
-  // Predicate to select cards with.  By default, no cards (in case we don't
-  // implement the virtual pack for some reason.)
-  var which = (card) => false;
+  showPack: function(packName) {
+    if (packName.indexOf("__") === 0)
+      this.showVirtualPack(packName);
+    else
+      this.showActualPack(packName);
+  },
 
-  if (packName === "__all") {
-    which = (card) => true;
-  } else if (packName === "__wrongs") {
-    which = (card) => card.errors.length > 0;
-  } else if (packName === "__multiple") {
-    // TODO: select multiple
-    which = (card) => true;
+  showVirtualPack: function(packName) {
+    // Predicate to select cards with.  By default, no cards (in case we don't
+    // implement the virtual pack for some reason.)
+    var which = (card) => false;
+
+    if (packName === "__all") {
+      which = (card) => true;
+    } else if (packName === "__wrongs") {
+      which = (card) => card.errors.length > 0;
+    } else if (packName === "__multiple") {
+      // TODO: select multiple
+      which = (card) => true;
+    }
+    Reviewer.reviewCards(Cards.withinPacks(PACKS).filter(which), packName);
+  },
+
+  showActualPack: function(packName) {
+    var which = (card) => card.pack_name === packName;
+    Reviewer.reviewCards(Cards.withinPacks(PACKS).filter(which), "basicFaces");
+  },
+
+  // Assumes PACKS global variable has been loaded.
+  loadPacksList: function() {
+    this.virtualPacks.forEach(pack => {
+      $("#virtual").append(this.buttonForPack(pack.title, pack.name));
+    });
+
+    PACKS.forEach(pack => { 
+      $("#actual").append(this.buttonForPack(pack[0].pack_name, pack[0].pack_name));
+    });
+
+    $(".packs").show();
   }
-  Reviewer.reviewCards(Cards.withinPacks(PACKS).filter(which), packName);
-}
-
-function showActualPack(packName) {
-  var which = (card) => card.pack_name === packName;
-  Reviewer.reviewCards(Cards.withinPacks(PACKS).filter(which), "basicFaces");
-}
-
-// Assumes PACKS global variable has been loaded.
-function loadPacksList() {
-  virtualPacks.forEach(function(pack) {
-    $("#virtual").append(buttonForPack(pack.title, pack.name));
-  });
-
-  PACKS.forEach(function(pack) {
-    $("#actual").append(buttonForPack(pack[0].pack_name, pack[0].pack_name));
-  });
-
-  $(".packs").show();
-}
+};
 
 $(function() {
   $("#review-next").click(() => Reviewer.reviewNext());
@@ -72,6 +76,6 @@ $(function() {
     var cardCompare = (c1, c2) => (c1.created_date < c2.created_date);
     var packCompare = (p1, p2) => cardCompare(p1[0], p2[0]);
     PACKS = Cards.groupedByPack(cards, cardCompare, packCompare);
-    loadPacksList();
+    PacksViewer.loadPacksList();
   });
 });
