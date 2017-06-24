7418f6ab49251b088df56235eb24f2d5843f3cae
Michael Gundlach <gundlach@gmail.com>
2017-06-24 17:47:56 +0000

    Caching works

--- inputter/cards.js
+++ inputter/cards.js
@@ -17,10 +17,11 @@ Cards = {
     },
 
     // Update |card|, calling callback() upon success.
-    update: function(card, callback) {
+    update: function(card, callback, failure) {
+      failure = failure || function() {};
       var data = JSON.stringify(card);
       $.ajax(this._root + card.id,
-             {type: "PUT", data: data, success: callback});
+             {type: "PUT", data: data, success: callback, error: failure});
     },
 
     // Delete the given card id, calling callback() upon success.
@@ -44,22 +45,72 @@ Cards = {
   // offline, and queues writes for when we're online.
   __cachingApi: {
     get_all: function(callback) {
-      console.log("TODO: caching");
-      Cards.api.__wrappedApi.get_all(callback);
+      if (!this._get('get_all')) {
+        // Very first time, just do a duplicate GET, rather than trying to
+        // be notified of the end of __initialize's call
+        this.__wrappedApi.get_all(callback);
+      } else {
+        callback(this._get('get_all'));
+      }
+    },
+    update: function(card, callback) {
+      var updates = this._get('update');
+      updates.push(card);
+      this._set('update', updates);
+      this.__clearUpdateQueue();
+      callback(card);
     },
     add: function(card, callback) {
       alert("Adding new cards in Cards.enable_caching() mode is not supported.");
     },
-    update: function(card, callback) {
-      console.log("TODO: caching");
-      Cards.api.__wrappedApi.update(card, callback);
-    },
     del: function(cardid, callback) {
-      console.log("TODO: caching");
-      Cards.api.__wrappedApi.del(cardid, callback);
+      alert("Deleting cards in Cards.enable_caching() mode is not yet supported.");
     },
     __initialize: function() {
-      console.log("TODO: caching");
-    }
+      if (!this._get('get_all'))
+        this._set('get_all', []);
+      this.__wrappedApi.get_all(cards => this._set('get_all', cards));
+
+      if (!this._get('update'))
+        this._set('update', []);
+      this.__clearUpdateQueue(); // in case any are left from last time
+    },
+    __clearUpdateQueue: function() {
+      var updates = this._get('update');
+      if (updates.length > 0) {
+        var card = updates.shift();
+        this._set('update', updates);
+        this.__wrappedApi.update(card, () => {
+          this.__clearUpdateQueue();
+        }, () => {
+          // Failure -- put it back on the front of the list
+          var updates = this._get('update');
+          updates.unshift(card);
+          this._set('update', updates);
+          // Check for network in a bit unless someone else already is
+          if (!this.__clearUpdateIsSleeping) {
+            this.__clearUpdateIsSleeping = true;
+            setTimeout(() => {
+              this.__clearUpdateIsSleeping = false;
+              this.__clearUpdateQueue();
+            }, 10000);
+          }
+        });
+      }
+    },
+
+    _get: function(key) {
+      var json = localStorage.getItem(key);
+      if (!json)
+        return undefined;
+      return JSON.parse(json);
+    },
+    _set: function(key, val) {
+      var json = JSON.stringify(val);
+      localStorage.setItem(key, json);
+    },
+
+    __wrappedApi: undefined
   }
 }
+
